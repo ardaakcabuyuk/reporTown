47fd732b38e17b9342235eaa08a039d681d9484e
ardaakcabuyuk <ardaakcabuyuk@gmail.com>
2021-12-22 01:19:25 +0300

    Created Java Spring Boot Project

---
+++ Java/src/main/java/com/senior/reporTown/ReporTownApplication.java
@@ -0,0 +1,13 @@
+package com.senior.reporTown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporTownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReporTownApplication.class, args);
+	}
+
+}
---
+++ Java/src/test/java/com/senior/reporTown/ReporTownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senior.reporTown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReporTownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
