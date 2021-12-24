12320d72d3f4ccfe0fa2918943487fbc9f2156d0
Oğuz Kaan İmamoğlu <60095076+OguzKaanImamoglu@users.noreply.github.com>
2021-12-24 20:44:38 +0300

    fronent created

---
+++ React_Native/reporTown/App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+
+export default function App() {
+
+    return <WelcomeScreen/>;
+    
+  }
