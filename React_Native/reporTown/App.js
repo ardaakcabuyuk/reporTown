084bfe1f9a7bd46ade754f5e846d7758959611fd
Oğuz Kaan İmamoğlu <60095076+OguzKaanImamoglu@users.noreply.github.com>
2021-12-25 15:34:14 +0300

    login screen

--- React_Native/reporTown/App.js
+++ React_Native/reporTown/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './app/screens/LoginScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
 
-    return <WelcomeScreen/>;
+    return <LoginScreen/>;
     
   }
