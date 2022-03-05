8b92dea0096b85a9133eeb9a64f83b4fd736ca65
ardaakcabuyuk <ardaakcabuyuk@gmail.com>
2022-03-05 15:37:54 +0300

    delete mongo exclusion

--- Java/src/main/java/com/senior/reporTown/ReporTownApplication.java
+++ Java/src/main/java/com/senior/reporTown/ReporTownApplication.java
@@ -2,9 +2,10 @@ package com.senior.reporTown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class})
+@SpringBootApplication
 public class ReporTownApplication {
 
 	public static void main(String[] args) {
